7ef47a5e23c37352b0042a61389ebd527edc5d10
Gobidev <adrian.groh@t-online.de>
2021-04-14 10:55:19 +0200

    Improve logging

--- EDNeutronAssistant.py
+++ EDNeutronAssistant.py
@@ -24,11 +24,15 @@ def parse_game_log() -> list:
     """Parses the Elite: Dangerous logfiles to retrieve information about the game"""
 
     # Check OS
+    print_log("Checking Host OS..")
+
     if os.name == "nt":
+        print_log("..Host OS is Windows, continuing..")
 
         # Retrieve newest log file
         windows_username = os.getlogin()
         file_directory = "C:\\Users\\" + windows_username + "\\Saved Games\\Frontier Developments\\Elite Dangerous"
+        print_log(f"Reading game log from {file_directory}")
         all_files = [file_directory + "\\" + n for n in os.listdir(file_directory)]
 
         # Filter files that are no logs
@@ -37,8 +41,10 @@ def parse_game_log() -> list:
                 del all_files[all_files.index(file)]
 
         newest_log_file = max(all_files, key=os.path.getctime)
+        print_log(f"Found newest log file {newest_log_file}")
 
         # Read log file
+        print("Reading log file..")
         with open(newest_log_file, "r") as f:
             all_entries = f.readlines()
 
@@ -46,15 +52,20 @@ def parse_game_log() -> list:
         for entry in all_entries:
             entries_parsed.append(eval(entry.replace("true", "True").replace("false", "False")))
 
+        print_log("..done")
+
         return entries_parsed
 
     else:
-        print("This method currently only supports Windows")
+        print_log("Not supported OS found, aborting..")
         return []
 
 
 def get_current_system(entries_parsed: list) -> str:
     """Return name of the last star system the commander visited"""
+
+    if VERBOSE:
+        print_log("Looking for current system..")
 
     all_session_systems = []
     for entry in entries_parsed:
@@ -64,19 +75,39 @@ def get_current_system(entries_parsed: list) -> str:
 
     if len(all_session_systems) > 0:
         current_system = all_session_systems[-1]
+        if VERBOSE:
+            print_log(f"..found system {current_system}")
     else:
         current_system = ""
+        if VERBOSE:
+            print("..no current system found")
+
     return current_system
 
 
 def get_commander_name(entries_parsed: list) -> str:
     """Parse log file for commander name"""
 
+    if VERBOSE:
+        print_log("Looking for commander name..")
+
+    commander_name = ""
+
     for entry in entries_parsed:
         if entry["event"] == "Commander":
-            return entry["Name"]
+            commander_name = entry["Name"]
+            break
         elif "Commander" in entry:
-            return entry["Commander"]
+            commander_name = entry["Commander"]
+            break
+        
+    if VERBOSE:
+        if commander_name:
+            print_log(f"..found name {commander_name}")
+        else:
+            print_log("..no commander name found")
+
+    return commander_name
 
 
 def parse_plotter_csv(filename: str) -> list:
@@ -110,10 +141,10 @@ def get_distance_between_systems(system1: str, system2: str) -> float:
     s1coordinates = get_coordinates(system1)
     s2coordinates = get_coordinates(system2)
 
-    distance = ((s2coordinates["x"] - s1coordinates["x"]) ** 2 + (s2coordinates["y"] - s1coordinates["y"]) ** 2 +
-                (s2coordinates["z"] - s1coordinates["z"]) ** 2) ** (1 / 2)
+    distance = round(((s2coordinates["x"] - s1coordinates["x"]) ** 2 + (s2coordinates["y"] - s1coordinates["y"]) ** 2 +
+                (s2coordinates["z"] - s1coordinates["z"]) ** 2) ** (1 / 2), 2)
 
-    return round(distance, 2)
+    return distance
 
 
 def get_nearest_system_in_route(plotter_data: list, current_system: str) -> str:
@@ -134,28 +165,34 @@ def get_nearest_system_in_route(plotter_data: list, current_system: str) -> str:
 def copy_system_to_clipboard(system: str):
     """Copy a system into the commanders clipboard"""
     clipboard.copy(system)
+    print_log(f"Copied system {system} to clipboard")
 
 
 def calc_simple_neutron_route(efficiency: int, ship_range: float,  start_system: str, end_system: str) -> list:
     """Use the Spansh API to calculate a neutron star route"""
+
+    print_log(f"Calculating route from {start_system} to {end_system} with efficiency {efficiency} and jump range {ship_range}..")
 
     filename = f"{CONFIG_PATH}\\NeutronAssistantRoute-{efficiency}-{ship_range}-" \
                f"{start_system.replace(' ', '_')}-{end_system.replace(' ', '_')}.route"
 
     # Test if route was calculated before
     if not os.path.isfile(filename):
-        print_log("Route was not calculated before, requesting from api..")
+        print_log("Route was not calculated before, requesting from API..")
 
         session = requests.Session()
         payload = {"efficiency": efficiency, "range": ship_range, "from": start_system, "to": end_system}
         response = session.post("https://www.spansh.co.uk/api/route", data=payload)
         job = eval(response.text)
 
+        print_log("Request sent, waiting for completion..")
+
         # Wait for job completion
         while 1:
             response = session.get("https://www.spansh.co.uk/api/results/" + job["job"])
             response_dict = eval(response.text.replace("true", "True").replace("false", "False"))
             if response_dict["status"] == "ok":
+                print_log("..Route successfully recieved")
                 break
             time.sleep(1)
         session.close()
@@ -163,8 +200,11 @@ def calc_simple_neutron_route(efficiency: int, ship_range: float,  start_system:
         systems = response_dict["result"]["system_jumps"]
 
         # Write results to file
+        print_log("Saving route..")
+
         with open(filename, "w") as f:
             f.write(str(systems))
+        print_log("..Route has been saved")
 
     else:
         print_log("Found existing route, reading..")
