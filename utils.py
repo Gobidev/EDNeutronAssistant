ed5edc087ff8c0f683486c86b244ee91217cd1e0
Gobidev <adrian.groh@t-online.de>
2021-04-30 21:55:24 +0200

    Add method to calculate exact route using spansh api

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import time
 import gzip
 import io
 import base64
+import hashlib
 
 
 def parse_game_log(log_function=print, verbose=False) -> list:
@@ -230,16 +231,15 @@ def calc_simple_neutron_route(efficiency: int, ship_range: float, start_system: 
     if not os.path.isdir(config_path + "\\routes"):
         os.mkdir(config_path + "\\routes")
 
-    filename = f"{config_path}\\routes\\NeutronAssistantRoute-{efficiency}-{ship_range}-" \
+    filename = f"{config_path}\\routes\\NeutronAssistantSimpleRoute-{efficiency}-{ship_range}-" \
                f"{start_system.replace(' ', '_')}-{end_system.replace(' ', '_')}.json"
 
     # Test if route was calculated before
     if not os.path.isfile(filename):
         log_function("Route was not calculated before, requesting from API")
 
-        session = requests.Session()
         payload = {"efficiency": efficiency, "range": ship_range, "from": start_system, "to": end_system}
-        response = session.post("https://www.spansh.co.uk/api/route", data=payload)
+        response = requests.post("https://www.spansh.co.uk/api/route", data=payload)
         job = eval(response.text)
 
         log_function("Request sent, waiting for completion")
@@ -250,15 +250,153 @@ def calc_simple_neutron_route(efficiency: int, ship_range: float, start_system: 
 
         # Wait for job completion
         while 1:
-            response = session.get("https://www.spansh.co.uk/api/results/" + job["job"])
+            response = requests.get("https://www.spansh.co.uk/api/results/" + job["job"])
             response_dict = json.loads(response.text)
             if response_dict["status"] == "ok":
                 log_function("Route successfully received")
                 break
             time.sleep(1)
-        session.close()
 
         systems = response_dict["result"]["system_jumps"]
+
+        # Write results to file
+        log_function("Saving route")
+
+        with open(filename, "w") as f:
+            json.dump(systems, f, indent=2)
+        log_function("Route has been saved")
+
+    else:
+        log_function("Found existing route, reading")
+        systems = json.load(open(filename, "r"))
+
+    return systems
+
+
+def calc_exact_neutron_route(start_system: str, end_system: str, ship_coriolis_build: dict, cargo: int,
+                             already_supercharged: bool, use_supercharge: bool, use_injections: bool,
+                             exclude_secondary_stars: bool, config_path: str, log_function=print) -> list:
+    """Use the Spansh API to calculate an exact neutron route"""
+
+    fsd_data = requests.get("https://raw.githubusercontent.com/EDCD/coriolis-data/"
+                            "master/modules/standard/frame_shift_drive.json").json()["fsd"]
+
+    def calculate_optimal_mass(coriolis_build: dict) -> float:
+        build_fsd = coriolis_build["components"]["standard"]["frameShiftDrive"]
+
+        optimal_mass = 0
+        for fsd in fsd_data:
+            if fsd["class"] == build_fsd["class"] and fsd["rating"] == build_fsd["rating"]:
+                optimal_mass = fsd["optmass"]
+
+        # test if modified
+        if "blueprint" in build_fsd:
+            modification_grade = build_fsd["blueprint"]["grade"]
+            multiplier = build_fsd["blueprint"]["grades"][str(modification_grade)]["features"]["optmass"][1]
+
+            # test for experimental effect
+            if "special" in build_fsd["blueprint"]:
+                if build_fsd["blueprint"]["special"]["name"] == "Mass Manager":
+                    multiplier += .062
+
+            optimal_mass *= multiplier + 1
+
+        return round(optimal_mass)
+
+    def get_fuel_power_of_build(coriolis_build: dict) -> float:
+        build_fsd = coriolis_build["components"]["standard"]["frameShiftDrive"]
+
+        fuel_power = 0
+        for fsd in fsd_data:
+            if fsd["class"] == build_fsd["class"] and fsd["rating"] == build_fsd["rating"]:
+                fuel_power = fsd["fuelpower"]
+
+        return fuel_power
+
+    def get_fuel_multiplier_of_build(coriolis_build: dict) -> float:
+        build_fsd = coriolis_build["components"]["standard"]["frameShiftDrive"]
+
+        fuel_multiplier = 0
+        for fsd in fsd_data:
+            if fsd["class"] == build_fsd["class"] and fsd["rating"] == build_fsd["rating"]:
+                fuel_multiplier = fsd["fuelmul"]
+
+        return fuel_multiplier
+
+    def get_fsd_fuel_usage_of_build(coriolis_build: dict) -> float:
+        build_fsd = coriolis_build["components"]["standard"]["frameShiftDrive"]
+
+        fsd_fuel_usage = 0
+        for fsd in fsd_data:
+            if fsd["class"] == build_fsd["class"] and fsd["rating"] == build_fsd["rating"]:
+                fsd_fuel_usage = fsd["maxfuel"]
+
+        return fsd_fuel_usage
+
+    def calculate_range_boost(coriolis_build: dict) -> float:
+        rating_boost_dict = {"1": 4.0, "2": 6.0, "3": 7.8, "4": 9.3, "5": 10.5}
+
+        boost = 0.0
+        for module in coriolis_build["components"]["internal"]:
+            if module and "group" in module and module["group"] == "Guardian Frame Shift Drive Booster":
+                boost = rating_boost_dict[str(module["class"])]
+
+        return boost
+
+    log_function(f"Calculating exact route from {start_system} to {end_system}")
+
+    if not os.path.isdir(config_path + "\\routes"):
+        os.mkdir(config_path + "\\routes")
+
+    ship_code_hash = hashlib.md5(ship_coriolis_build["references"][0]["code"].encode("utf-8")).hexdigest()[:5]
+    filename = f"{config_path}\\routes\\NeutronAssistantExactRoute--{start_system.replace(' ', '_')}-" \
+               f"{end_system.replace(' ', '_')}-{ship_code_hash}-{cargo}-{'Y' if already_supercharged else 'N'}-" \
+               f"{'Y' if use_supercharge else 'N'}-{'Y' if use_injections else 'N'}-" \
+               f"{'Y' if exclude_secondary_stars else 'N'}.json "
+
+    # Test if route was calculated before
+    if not os.path.isfile(filename):
+        log_function("Route was not calculated before, requesting from API")
+        log_function("This might take a while")
+
+        payload = {
+            "source": start_system,
+            "destination": end_system,
+            "is_supercharged": 1 if already_supercharged else 0,
+            "use_supercharge": 1 if use_supercharge else 0,
+            "exclude_secondary": 1 if exclude_secondary_stars else 0,
+            "tank_size": ship_coriolis_build["stats"]["fuelCapacity"],
+            "cargo": cargo,
+            "optimal_mass": calculate_optimal_mass(ship_coriolis_build),
+            "base_mass": ship_coriolis_build["stats"]["unladenMass"] + ship_coriolis_build["stats"][
+                "reserveFuelCapacity"],
+            "internal_tank_size": ship_coriolis_build["stats"]["reserveFuelCapacity"],
+            "max_fuel_per_jump": get_fsd_fuel_usage_of_build(ship_coriolis_build),
+            "range_boost": calculate_range_boost(ship_coriolis_build),
+            "fuel_power": get_fuel_power_of_build(ship_coriolis_build),
+            "fuel_multiplier": get_fuel_multiplier_of_build(ship_coriolis_build),
+            "ship_build": ship_coriolis_build
+        }
+
+        response = requests.post("https://www.spansh.co.uk/api/generic/route", data=payload)
+        job = eval(response.text)
+
+        log_function("Request sent, waiting for completion")
+
+        if "error" in job:
+            log_function(f"ERROR OCCURRED: {job['error']}")
+            return []
+
+        # Wait for job completion
+        while 1:
+            response = requests.get("https://www.spansh.co.uk/api/results/" + job["job"])
+            response_dict = json.loads(response.text)
+            if response_dict["status"] == "ok":
+                log_function("Route successfully received")
+                break
+            time.sleep(4)
+
+        systems = response_dict["result"]["jumps"]
 
         # Write results to file
         log_function("Saving route")
